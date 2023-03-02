1da0c0d1f48e5cff690cd332cfebe2e39a4f2b6a
leogimeness <leogimeness@outlook.com>
2023-03-02 15:36:18 -0300

    setting up methodOverride

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const express = require('express');
 const path = require('path')
 const app = express();
 const logMiddleWare = require("./middlewares/logSite")
+const methodOverride = require('method-override');
+
 
 
 const router = require('./router');
 
 app.set('model view engine','ejs')
 
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended:false}));
 app.use(logMiddleWare)
+app.use(methodOverride('_method'));
 
 app.use(router)
 
