bea1db3e2a08046422ac879ec8586e4fae234dbd
leogimeness <leogimeness@outlook.com>
2023-03-16 12:15:56 -0300

    creating/storaging cart items + summing quantity

--- middlewares/globalSetter.js
+++ middlewares/globalSetter.js
@@ -1,7 +1,24 @@
-function globalSetter (req,res,next){
+function globalSetter(req, res, next) {
     // console.log(req.session)
     res.locals.username = req.session.username
-    // console.log(res.locals)
+    res.locals.cart = req.session.cart
+
+    let totItemCart = 0
+    let totValue = 0
+
+    if (req.session.cart) {
+
+
+        req.session.cart.forEach((item) => {
+            totItemCart += item.quantity;
+            totValue += Number(item.total);
+        })
+
+
+    }
+    res.locals.totItemCart = totItemCart;
+    res.locals.totValue = totValue;
+
     next();
 }
 
