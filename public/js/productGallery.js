ad5dcefcd86e606e2ae4600a5a0ef54b7c644f7b
leogimeness <leogimeness@outlook.com>
2023-04-12 17:15:54 -0300

    adding click function on arrows and images

--- public/js/productGallery.js
+++ public/js/productGallery.js
@@ -1,4 +1,51 @@
-const imageGallery = document.querySelectorAll('.image-gallery');
+const galleryItems = document.querySelectorAll('.carrousel-item');
+const leftArrow = document.querySelector('#left-Arr');
+const rightArrow = document.querySelector('#right-Arr');
+const itemElement = document.querySelector('.mainImage');
+const galleryElement = document.querySelector('.image-gallery');
 
-const leftArrow = document.querySelector('#left-arr');
-const rightArrow = document.querySelector('#right-arr');
+console.log(galleryItems)
+
+let currentIndex = 0;
+
+leftArrow.addEventListener('click', () => {
+
+    currentIndex = (currentIndex - 1 + galleryItems.length) % galleryItems.length;
+    displayImage();
+});
+
+rightArrow.addEventListener('click', () => {
+
+    currentIndex = (currentIndex + 1) % galleryItems.length;
+    displayImage();
+});
+
+
+for (let element of galleryItems) {
+    element.addEventListener('click', (onGalleryItemClick))
+
+}
+
+function displayImage() {
+    
+    for (let i = 0; i < galleryItems.length; i++) {
+        if(i == currentIndex){
+            itemElement.appendChild(galleryItems[i])
+        } else{
+            galleryElement.appendChild(galleryItems[i])
+        }
+    }
+}
+
+function onGalleryItemClick(evt){
+    let itemCliked = evt.target
+    for (let i = 0; i < galleryItems.length; i++){
+        if(itemCliked == galleryItems[i]){
+            currentIndex = i
+        }
+    }
+
+    displayImage()
+    console.log(evt.target)
+
+}
