5f57c486a3ce633607362ba651c8042709f2e85c
leogimeness <leogimeness@outlook.com>
2023-04-14 13:46:50 -0300

    sending the logout sucessfully message for the view

--- controller/loginController.js
+++ controller/loginController.js
@@ -2,22 +2,22 @@ const users = require("../database/usuarios.json");
 const admUsers = require('../database/admUsers.json')
 
 const loginController = {
-    verifyAccount:(req,res)=>{
-        let {email,password} = req.body
+    verifyAccount: (req, res) => {
+        let { email, password } = req.body
 
         let user = users.find(u => u.email.toLocaleLowerCase() == email.toLocaleLowerCase() && u.password == password);
 
-        if(user == undefined){
+        if (user == undefined) {
             return res.redirect('/sign-in');
         };
 
         req.session.logInVerified = true;
         req.session.username = user.firstName;
         res.redirect('/')
-        
+
     },
-    verifyAdmAccount:(req,res) =>{
-        let {email,password} = req.body
+    verifyAdmAccount: (req, res) => {
+        let { email, password } = req.body
 
         console.log(email)
         console.log(password)
@@ -26,7 +26,7 @@ const loginController = {
 
         console.log(admUser)
 
-        if(admUser == undefined){
+        if (admUser == undefined) {
             return res.redirect('/adm/login')
         };
 
@@ -35,16 +35,20 @@ const loginController = {
 
         res.redirect('/adm')
     },
-    logout:(req,res) =>{
-        
+    logout: (req, res) => {
+
         req.session.destroy((err) => {
             if (err) {
-              console.log(err);
+                console.log(err);
             } else {
-              // redirect the user to the sign-in page
-              res.redirect('/sign-in');
+                const logOutMessage = 'You have been logged out successfully.'
+                res.locals.logOutMessage = logOutMessage;
+
+                console.log('message:', logOutMessage)
+                // redirect the user to the sign-in page
+                res.render('sign-in.ejs',{logOutMessage});
             }
-          });
+        });
     }
 
 }
